61dda6554895ae97284da8bc21aa0ae3dcfcc109
9akaga <9akaga@gmail.com>
2022-10-16 01:55:58 +0800

    更新添加badge的方法

--- playground.py
+++ playground.py
@@ -2,8 +2,7 @@ import os
 from PIL import Image, ImageFont, ImageDraw
 
 if __name__ == '__main__':
-    list = [1, 2, 3]
-    for i in range(len(list)):
-        list[i] = list[i] + 1
+    list = [(1, 1),
+            (1, 2)]
 
-    print(list)
+    print(list[1][1])
